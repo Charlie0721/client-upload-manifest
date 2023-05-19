72fb4241438d58dce7262e94354bce800e4af19d
Carlos Casas <carloscasasramirez@gmail.com>
2023-05-19 13:21:19 -0500

    change

--- src/components/Manifest/GetManifest.vue
+++ src/components/Manifest/GetManifest.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="container" style="padding-top: 50px">
+    <div class="card">
+      <div class="card-body">
+        <h1 class="h2 text-center">MANIFIESTOS: {{ manifestPosId.join(', ') }}</h1>
+      </div>
+    </div>
+    <br /><br />
     <div class="row">
       <div class="col-md-4 mb-4" v-for="(file, index) in files" :key="file._id + index">
         <div class="card text-white bg-dark">
           <div class="card-header">
-            <h5 class="card-title text-center">     Manifiesto: {{ file.manifestPosId[0] }}</h5>
-  </div>
-         <div class="card-content">
-      
+            <h5 class="card-title text-center"> Manifiesto: {{ file.manifestPosId[0] }}</h5>
+          </div>
+          <div class="card-content">
+
             <p class="card-text">
               Url PDF: <a class="text-white" :href="file.imageURL" target="_blank">{{ file.imageURL }}</a>
             </p>
@@ -19,24 +25,26 @@
 </template>
   
 <script lang="ts">
-import { defineComponent, computed, onMounted } from 'vue'
+import { defineComponent, computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useFileStore } from '../../stores/getFilesByProductsId.store'
 
 export default defineComponent({
-    setup() {
+  setup() {
     const fileStore = useFileStore()
     const route = useRoute()
-    const manifestPosId = (route.params.manifestPosId as string).split(',')
+    const manifestPosId = ref<string[]>([])
 
-    //@ts-ignore
+    manifestPosId.value = (route.params.manifestPosId as string).split(',')
 
     const files = computed(() => fileStore.files)
+
     onMounted(async () => {
-      await fileStore.fetchFiles(manifestPosId.join(','))
+      await fileStore.fetchFiles(manifestPosId.value.join(','))
     })
 
     return {
+      manifestPosId,
       files
     }
   }
